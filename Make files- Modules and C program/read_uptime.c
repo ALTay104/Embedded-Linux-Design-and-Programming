51376365fe2fc49a6f4fec23c86e44f825750ee1
ALT <ailitay104@gmail.com>
2018-06-04 22:44:06 -0700

    Add files via upload

---
+++ Make files- Modules and C program/read_uptime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+  int fd;
+  int rc = 0;
+  char rd_buf[10];
+
+  printf("Program Starts: read uptime and display every 10 seconds:\n");
+  while(1) {
+	wait (10);
+    fd = open("/proc/jiffies_count",O_RDWR);
+    if(fd == -1) {
+      perror("open proc file failed");
+      exit(-1);
+    }
+    printf("open proc file succeeded\n");
+    rc = read(fd,rd_buf,80);
+    close(fd);
+    printf("Uptime: %s seconds \n",rd_buf);
+    sleep((unsigned int)5);
+  }
+  return 0;
+}
